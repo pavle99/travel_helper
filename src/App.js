b1c55ea9dccb1c03db727abc70620a29c0ea9a99
pavle99 <mi18024@alas.matf.bg.ac.rs>
2022-06-22 13:58:38 +0200

    create project skeleton

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React from "react";
+import { CssBaseline, Grid } from "@material-ui/core";
+
+
+import Header from "./components/Header";
+import List from "./components/List";
+import Map from "./components/Map";
 
 const App = () => {
-    return (
-        <div>
-            <h1>HEllo</h1>
-        </div>
-    );
+  return (
+    <>
+      <CssBaseline/>
+      <Header />
+      <Grid container spacing={3} style={{ width: "100%" }}>
+        <Grid item xs={12} md={4}>
+          <List />
+        </Grid>
+        <Grid
+          item
+          xs={12}
+          md={8}
+        >
+          <Map/>
+        </Grid>
+      </Grid>
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/LocationCard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LocationCard = () => {
+  return (
+    <div>
+      card
+    </div>
+  );
+};
+
+export default LocationCard;
