2c15c873d55da5c9ef0ab4d1a5ddfad129b7b709
pavle99 <mi18024@alas.matf.bg.ac.rs>
2022-06-23 13:09:01 +0200

    add Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,38 @@
 import React from "react";
+import { AppBar, Box, InputBase, Toolbar, Typography } from "@material-ui/core";
+import { Autocomplete } from "@material-ui/lab";
+
+import SearchIcon from "@material-ui/icons/Search";
+
+import useStyles from "./styles.js";
 
 const Header = () => {
+  const styles = useStyles();
+
   return (
-    <div>
-      header
-    </div>
+    <AppBar position="static">
+      <Toolbar className={styles.toolbar}>
+        <Typography variant="h5" className={styles.title}>
+          Travel Advisor
+        </Typography>
+        <Box display="flex">
+          <Typography variant="h6" className={styles.title}>
+            Explore new places
+          </Typography>
+          {/*<Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}>*/}
+            <div className={styles.search}>
+              <div className={styles.searchIcon}>
+                <SearchIcon/>
+              </div>
+              <InputBase
+                placeholder="Search…"
+                classes={{ root: styles.inputRoot, input: styles.inputInput }}
+              />
+            </div>
+          {/*</Autocomplete>*/}
+        </Box>
+      </Toolbar>
+    </AppBar>
   );
 };
 
