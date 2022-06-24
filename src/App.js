0114fd562d4fbdf088bdcf029b6f2a6f637333ef
pavle99 <mi18024@alas.matf.bg.ac.rs>
2022-06-24 15:03:14 +0200

    add List component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { CssBaseline, Grid } from "@material-ui/core";
 
 
@@ -7,13 +7,22 @@ import List from "./components/List";
 import Map from "./components/Map";
 
 const App = () => {
+
+  const [type, setType] = useState("restaurants");
+  const [rating, setRating] = useState("");
+
   return (
     <>
       <CssBaseline/>
       <Header />
       <Grid container spacing={3} style={{ width: "100%" }}>
         <Grid item xs={12} md={4}>
-          <List />
+          <List
+            type={type}
+            setType={setType}
+            rating={rating}
+            setRating={setRating}
+          />
         </Grid>
         <Grid
           item
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,9 +1,31 @@
 import React from "react";
+import { CircularProgress, FormControl, Grid, InputLabel, MenuItem, Select, Typography } from "@material-ui/core";
 
-const List = () => {
+import useStyles from "./styles.js";
+
+const List = ({ type, setType, rating, setRating }) => {
+  const styles = useStyles();
+
   return (
-    <div>
-      list
+    <div className={styles.container}>
+      <Typography variant="h4">Restaurants, Hotels & Attractions around you</Typography>
+      <FormControl className={styles.formControl}>
+        <InputLabel>Type</InputLabel>
+        <Select value={type} onChange={(e) => setType(e.target.value)}>
+          <MenuItem value="restaurants">Restaurants</MenuItem>
+          <MenuItem value="hotels">Hotels</MenuItem>
+          <MenuItem value="attractions">Attractions</MenuItem>
+        </Select>
+      </FormControl>
+      <FormControl className={styles.formControl}>
+        <InputLabel>Rating</InputLabel>
+        <Select value={rating} onChange={(e) => setRating(e.target.value)}>
+          <MenuItem value={0}>All</MenuItem>
+          <MenuItem value={3}>Above 3.0</MenuItem>
+          <MenuItem value={4}>Above 4.0</MenuItem>
+          <MenuItem value={4.5}>Above 4.5</MenuItem>
+        </Select>
+      </FormControl>
     </div>
   );
 };
