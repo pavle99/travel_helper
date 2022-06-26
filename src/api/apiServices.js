3dc1fe7988a84a114fd108bab9769b8b587f36d6
pavle99 <mi18024@alas.matf.bg.ac.rs>
2022-06-26 15:37:29 +0200

    connect to location provider API

--- src/api/apiServices.js
+++ src/api/apiServices.js
@@ -1,1 +1,23 @@
-rsc
+import axios from "axios";
+
+export const getLocationData = async (type, sw, ne) => {
+  try {
+    const { data: { data } } = await axios.get(`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`, {
+      params: {
+        bl_latitude: sw.lat,
+        tr_latitude: ne.lat,
+        bl_longitude: sw.lng,
+        tr_longitude: ne.lng
+      },
+      headers: {
+        "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+        "X-RapidAPI-Host": "travel-advisor.p.rapidapi.com"
+      }
+    });
+
+    return data;
+  } catch (error) {
+    console.log(process.env.REACT_APP_RAPID_API_KEY)
+    console.log(error);
+  }
+};
