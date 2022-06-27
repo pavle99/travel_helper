0502c604ee4c92288573f3ebbd006bc524ec3101
pavle99 <mi18024@alas.matf.bg.ac.rs>
2022-06-27 12:02:37 +0200

    connect to weather provider API

--- src/api/apiServices.js
+++ src/api/apiServices.js
@@ -21,3 +21,22 @@ export const getLocationData = async (type, sw, ne) => {
     console.log(error);
   }
 };
+
+export const getWeatherData = async (lat, lng) => {
+  try {
+    const { data } = await axios.get("https://community-open-weather-map.p.rapidapi.com/find", {
+      params: {
+        lon: lng,
+        lat: lat,
+      },
+      headers: {
+        "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+        "X-RapidAPI-Host": "community-open-weather-map.p.rapidapi.com"
+      }
+    });
+
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
